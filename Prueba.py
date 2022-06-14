94741a267fa440fba32f21b677548fa2a77c91ac
mvledesma <mledesma@clave.com>
2022-06-14 13:05:39 -0300

    Se agrega nuevo archivo

---
+++ Prueba.py
@@ -0,0 +1,29 @@
+#Es una prueba
+
+print("Hello")
+print(2+4)
+print(type("Hello"))
+
+
+#List
+
+print([10,20,30])
+print(["Hello","es una prueba", 20, 40.5, True])
+
+#Dictionaries (Clave, valor)
+
+print({
+    "name": "Marina",
+    "surname": "Ledesma",
+    "age": 42
+})
+
+#Variable
+
+name= "Marina"
+age=20
+
+print(dir(name))
+print(name.count("a"))
+print(name, age)
+print(name.upper())
